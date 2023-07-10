2f97393d7fc55bc58a007814645dc3edc080a287
talent943 <talentpachena40@gmail.com>
2023-07-10 15:48:02 +0200

    modified contact page

--- submit-enrolment.php
+++ submit-enrolment.php
@@ -1,36 +1,34 @@
 <?php
-    if(isset($_POST['email']) && $_POST['email'] != "") {
-        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-            // Get data from form 
-            $name = $_POST['name'];
-            $email= $_POST['email'];
-            $course= $_POST['course'];
-            $message= $_POST['message'];
-            
-            $to = "talentpachena40@gmail.com";
-            $subject = "This is the subject line";
-            
-            // The following text will be sent
-            // Name = user entered name
-            // Email = user entered email
-            // Message = user entered message
-            $txt ="Name = ". $name . "\r\n  Email = "
-                . $email . "\r\n  Course = "
-                . $course . "\r\n Message =" . $message;
-            
-            $headers = "From: noreply@demosite.com" . "\r\n" .
-                        "CC: somebodyelse@example.com";
-            if($email != NULL) {
-                mail($to, $subject, $txt, $headers);
-            }
-
-            $message_sent = true;
-        }
-        else{
-            $message_sent = false;
-        }
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+ 
+require 'vendor/autoload.php';
+ 
+$mail = new PHPMailer(true);
+ 
+try {
+    $mail->SMTPDebug = 2;                                      
+    $mail->isSMTP();                                           
+    $mail->Host       = 'smtp.gfg.com;';                   
+    $mail->SMTPAuth   = true;                            
+    $mail->Username   = 'user@gfg.com';                
+    $mail->Password   = 'password';                       
+    $mail->SMTPSecure = 'tls';                             
+    $mail->Port       = 587; 
+ 
+    $mail->setFrom('from@gfg.com', 'Name');          
+    $mail->addAddress('talentpachena40@gmail.com');
+    $mail->addAddress('receiver2@gfg.com', 'Name');
+      
+    $mail->isHTML(true);                                 
+    $mail->Subject = 'Subject';
+    $mail->Body    = 'HTML message body in <b>bold</b> ';
+    $mail->AltBody = 'Body in plain text for non-HTML mail clients';
+    $mail->send();
+    echo "Mail has been sent successfully!";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-    
-    // Redirect to
-    header("Location:sent.html");
+ 
 ?>
